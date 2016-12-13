855dfa413232534b469705d11c4640a94788b196
kev2018 <8kludwig@stpatrick.org>
2016-12-13 13:58:42 -0600

    finished cart

--- SemesterFinal-master/cart.js
+++ SemesterFinal-master/cart.js
@@ -15,8 +15,8 @@ class Cart{
   constructor(List,Quant){
     this.itemList = List;// array
     this.itemQuantity = Quant;// ditto
-    this.itemList = [];
-    this.itemQuantity = [];
+    this.itemList = [List];
+    this.itemQuantity = [Quant];
   };
   additem(i,q){
 this.itemList.push(i);
@@ -25,16 +25,25 @@ this.itemQuantity.push(q);
   totalCart(){
     let total = 0;
 for(let c =0;c<this.itemList.length;c++){
-let item =this.itemList[c].price;
+let item =(this.itemList[c].price)*this.itemQuantity[c];
 total = total + item;
 //return total;
 console.log(total);
 }
   };
-  subCart(days){
-
+  subCart(d){
+    let subcart = [];
+for(let t=0;t<this.itemList.length;t++){
+  if(this.itemList[t].shipping == d){
+    subcart.push(this.itemList[t]);
+  }
+  else{
+    console.log('no item matches your search.');
+  }
+}
   };
 }
 let amazon = new Cart(Nerf,1);
-//amazon.additem(hotwheels,2);
+amazon.additem(hotwheels,2);
+amazon.additem(Pants,1);
 amazon.totalCart();
